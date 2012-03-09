ac7823d39fd8d2efcd17c57cf5d8fc4c4ec359cb
Mauricio Sanches <mauricio.silveira.sanches@gmail.com>
2012-03-08 21:34:24 -0300

    Install does not change default shell anymore. If .oh-my-zsh dir exists, just fetch from git. Create personal zshrc file and personal theme.

---
+++ themes/bauricio.zsh-theme
@@ -0,0 +1,28 @@
+local user='%{$fg[blue]%}%m%{$reset_color%}'
+local pwd='%{$fg[green]%}%~%{$reset_color%}'
+local prompt_separator='%{$fg[red]%}:'
+local rvm=''
+if which rvm-prompt &> /dev/null; then
+  rvm='%{$fg[green]%}‹$(rvm-prompt i v g)›%{$reset_color%}'
+else
+  if which rbenv &> /dev/null; then
+    rvm='%{$fg[green]%}‹$(rbenv version | sed -e "s/ (set.*$//")›%{$reset_color%}'
+  fi
+fi
+local return_code='%(?..%{$fg[red]%}%? ↵%{$reset_color%})'
+local git_branch='$(git_prompt_status)%{$reset_color%}$(git_prompt_info)%{$reset_color%}'
+
+ZSH_THEME_GIT_PROMPT_PREFIX="%{$fg[red]%}"
+ZSH_THEME_GIT_PROMPT_SUFFIX="%{$reset_color%}"
+ZSH_THEME_GIT_PROMPT_DIRTY=""
+ZSH_THEME_GIT_PROMPT_CLEAN=""
+
+ZSH_THEME_GIT_PROMPT_ADDED="%{$fg[green]%}a "
+ZSH_THEME_GIT_PROMPT_MODIFIED="%{$fg[blue]%}m "
+ZSH_THEME_GIT_PROMPT_DELETED="%{$fg[red]%}d "
+ZSH_THEME_GIT_PROMPT_RENAMED="%{$fg[magenta]%}r "
+ZSH_THEME_GIT_PROMPT_UNMERGED="%{$fg[yellow]%}um "
+ZSH_THEME_GIT_PROMPT_UNTRACKED="%{$fg[cyan]%}ut "
+
+PROMPT="${user} ${pwd} ${prompt_separator} "
+RPROMPT="${return_code} ${git_branch} ${rvm}"
--- tools/install.sh
+++ tools/install.sh
@@ -1,11 +1,11 @@
 if [ -d ~/.oh-my-zsh ]
 then
-  echo "\033[0;33mYou already have Oh My Zsh installed.\033[0m You'll need to remove ~/.oh-my-zsh if you want to install"
-  exit
+  cd ~/.oh-my-zsh
+  git fetch
+else
+  echo "\033[0;34mCloning Oh My Zsh...\033[0m"
+  /usr/bin/env git clone https://github.com/bauricio/oh-my-zsh.git ~/.oh-my-zsh
 fi
-
-echo "\033[0;34mCloning Oh My Zsh...\033[0m"
-/usr/bin/env git clone https://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
 
 echo "\033[0;34mLooking for an existing zsh config...\033[0m"
 if [ -f ~/.zshrc ] || [ -h ~/.zshrc ]
@@ -16,13 +16,10 @@ then
 fi
 
 echo "\033[0;34mUsing the Oh My Zsh template file and adding it to ~/.zshrc\033[0m"
-cp ~/.oh-my-zsh/templates/zshrc.zsh-template ~/.zshrc
+ln -s  ~/.oh-my-zsh/templates/zshrc ~/.zshrc
 
 echo "\033[0;34mCopying your current PATH and adding it to the end of ~/.zshrc for you.\033[0m"
 echo "export PATH=$PATH" >> ~/.zshrc
-
-echo "\033[0;34mTime to change your default shell to zsh!\033[0m"
-chsh -s `which zsh`
 
 echo "\033[0;32m"'         __                                     __   '"\033[0m"
 echo "\033[0;32m"'  ____  / /_     ____ ___  __  __   ____  _____/ /_  '"\033[0m"
